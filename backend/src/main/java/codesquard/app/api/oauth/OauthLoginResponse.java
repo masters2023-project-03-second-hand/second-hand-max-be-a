9429232c1a14947ea8e4370f0e213f61b6250280
yonghwankim <dragonbead95@naver.com>
2023-08-23 19:23:23 +0900

    #5 feat: 로그인 요청에 대한 Request/Response 클래스 정의

--- backend/src/main/java/codesquard/app/api/oauth/OauthLoginResponse.java
+++ backend/src/main/java/codesquard/app/api/oauth/OauthLoginResponse.java
@@ -1,5 +1,14 @@
 package codesquard.app.api.oauth;
 
-public class OauthLoginResponse {
+import codesquard.app.domain.jwt.Jwt;
+import codesquard.app.domain.member.Member;
 
+public class OauthLoginResponse {
+	private final Jwt jwt;
+	private final Member user;
+
+	public OauthLoginResponse(Jwt jwt, Member user) {
+		this.jwt = jwt;
+		this.user = user;
+	}
 }
