856ebeb10624ab89c0d0dca9fbf4df32c485b5e7
yonghwankim <dragonbead95@naver.com>
2023-08-23 14:14:19 +0900

    #5 docs: mysql 컨테이너에 데이터베이스 설정 추가

--- backend/src/main/java/codesquard/app/domain/member/entity/Member.java
+++ backend/src/main/java/codesquard/app/domain/member/entity/Member.java
@@ -1,6 +1,14 @@
 package codesquard.app.domain.member.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String avatarUrl;
 	private String social_login_id;
