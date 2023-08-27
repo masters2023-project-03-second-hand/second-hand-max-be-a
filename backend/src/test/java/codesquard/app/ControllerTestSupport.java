c31b6432922a1b203b92697d43fc62ff73558542
yonghwankim <dragonbead95@naver.com>
2023-08-27 13:06:11 +0900

    #5 test: 단위 테스트, 통합 테스트를 도와주는 서포트 테스트 클래스 추가
    - 해당 클래스의 필드멤버에 필요한 객체들을 정의하여 사용합니다.

--- backend/src/test/java/codesquard/app/ControllerTestSupport.java
+++ backend/src/test/java/codesquard/app/ControllerTestSupport.java
@@ -1,7 +1,10 @@
 package codesquard.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import codesquard.app.api.oauth.OauthRestController;
 import codesquard.app.api.oauth.OauthService;
@@ -10,6 +13,10 @@ import codesquard.app.api.oauth.OauthService;
 	OauthRestController.class
 )
 public abstract class ControllerTestSupport {
+
+	@Autowired
+	protected ObjectMapper objectMapper;
+
 	@MockBean
 	protected OauthService oauthService;
 }
--- backend/src/test/java/codesquard/app/IntegrationTestSupport.java
+++ backend/src/test/java/codesquard/app/IntegrationTestSupport.java
@@ -1,7 +1,17 @@
 package codesquard.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import codesquard.app.api.oauth.OauthService;
+import codesquard.app.domain.member.MemberRepository;
 
 @SpringBootTest
 public abstract class IntegrationTestSupport {
+
+	@Autowired
+	protected OauthService oauthService;
+
+	@Autowired
+	protected MemberRepository memberRepository;
 }
