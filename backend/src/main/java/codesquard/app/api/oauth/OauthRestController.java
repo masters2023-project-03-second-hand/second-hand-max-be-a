ae7b15741a7c6b8f9cdcb24406592d0fe252de72
yonghwankim <dragonbead95@naver.com>
2023-08-27 12:46:10 +0900

    #5 feat: signup 서비스 구현
    - 회원가입 서비스는 클라이언트에게 로그인 아이디, 프로필 사진, 소셜 로그인에 대한 인가 코드를 받아 회원가입을 진행하는 서비스입니다.

--- backend/src/main/java/codesquard/app/api/oauth/OauthRestController.java
+++ backend/src/main/java/codesquard/app/api/oauth/OauthRestController.java
@@ -1,13 +1,22 @@
 package codesquard.app.api.oauth;
 
+import static org.springframework.http.HttpStatus.*;
+
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import codesquard.app.api.oauth.request.OauthSignUpRequest;
+import codesquard.app.api.oauth.response.OauthSignUpResponse;
 import codesquard.app.api.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -16,12 +25,20 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class OauthRestController {
 
+	private static final Logger log = LoggerFactory.getLogger(OauthRestController.class);
+
 	private final OauthService oauthService;
 
-	@PostMapping("/login")
-	public ApiResponse<OauthLoginResponse> login(@Valid @RequestBody OauthLoginRequest request,
-		@RequestParam String code) {
-		OauthLoginResponse response = oauthService.login(request, code);
-		return ApiResponse.ok(response);
+	@PostMapping("/{provider}/signup")
+	public ResponseEntity<ApiResponse<OauthSignUpResponse>> signUp(
+		@PathVariable(value = "provider") String provider,
+		@RequestParam String code,
+		@RequestPart(value = "profile", required = false) MultipartFile profile,
+		@Valid @RequestPart(value = "signupData") OauthSignUpRequest request) {
+		log.info("provider : {}, code : {}, profile : {}, request : {}", provider, code, profile, request);
+
+		OauthSignUpResponse response = oauthService.signUp(request, provider, code);
+		return ResponseEntity.status(CREATED)
+			.body(ApiResponse.created(response));
 	}
 }
--- backend/src/main/java/codesquard/app/api/oauth/OauthService.java
+++ backend/src/main/java/codesquard/app/api/oauth/OauthService.java
@@ -1,20 +1,54 @@
 package codesquard.app.api.oauth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import codesquard.app.domain.jwt.Jwt;
+import codesquard.app.api.oauth.client.OauthClient;
+import codesquard.app.api.oauth.request.OauthSignUpRequest;
+import codesquard.app.api.oauth.response.OauthAccessTokenResponse;
+import codesquard.app.api.oauth.response.OauthSignUpResponse;
+import codesquard.app.api.oauth.response.OauthUserProfileResponse;
 import codesquard.app.domain.member.Member;
+import codesquard.app.domain.member.MemberRepository;
+import codesquard.app.domain.oauth.OauthProvider;
+import codesquard.app.domain.oauth.repository.ProviderRepository;
+import lombok.RequiredArgsConstructor;
 
+@Transactional
+@RequiredArgsConstructor
 @Service
 public class OauthService {
 
-	public OauthLoginResponse login(OauthLoginRequest request, String code) {
-		// 1. 인가 코드를 이용하여 Oauth 서버에 accessToken 발급
-		// 2. accessToken을 이용하여 리소스 서버에 유저 프로필 정보 가져오기
-		// 3. loginId와 socalLoginId가 각각의 정보와 일치하는지 조회하고 Member 객체 반환
-		// 4. 인증된 회원 객체를 이용하여 JWT 객체 생성
-		// 5. redis 서버에 refreshToken 갱신(저장 또는 업데이트)
-		// 6. Response에 인증된 회원과 JWT 객체를 저장하여 반환
-		return new OauthLoginResponse(new Jwt(), new Member());
+	private static final Logger log = LoggerFactory.getLogger(OauthService.class);
+
+	private final ProviderRepository providerRepository;
+	private final MemberRepository memberRepository;
+	private final OauthClient oauthClient;
+
+	public OauthSignUpResponse signUp(OauthSignUpRequest request, String provider, String authorizationCode) {
+		log.info("OauthSignUpRequest : {}, provider : {}, authorizationCode : {}", request, provider,
+			authorizationCode);
+
+		// provider(naver, github, google...)등에 따른 oauth 정보를 가져온다
+		OauthProvider oauthProvider = providerRepository.findByProviderName(provider);
+		log.debug("oauthProvider : {}", oauthProvider);
+
+		// authorizationCode를 가지고 Oauth 서버에 요청하여 accessToken을 발급받는다
+		OauthAccessTokenResponse accessTokenResponse =
+			oauthClient.exchangeAccessTokenByAuthorizationCode(oauthProvider, authorizationCode);
+		log.debug("OauthAccessTokenResponse : {}", accessTokenResponse);
+
+		// 발급받은 accessToken을 이용하여 유저 프로필 정보를 가져온다
+		OauthUserProfileResponse userProfileResponse =
+			oauthClient.getUserProfileByAccessToken(provider, oauthProvider, accessTokenResponse);
+		log.debug("userProfileResponse : {}", userProfileResponse);
+
+		// 회원 정보를 DB에 저장
+		Member member = request.toEntity(userProfileResponse.getSocialLoginId());
+		Member saveMember = memberRepository.save(member);
+
+		return OauthSignUpResponse.of(saveMember);
 	}
 }
