5068897b57773d9f299c784d48b51324a6db7536
yonghwankim <dragonbead95@naver.com>
2023-08-27 13:04:26 +0900

    #5 feat: 회원 저장소에 findMemberByNicknameIs 추가
    - 닉네임에 따른 회원을 조회합니다.

--- backend/src/main/java/codesquard/app/domain/member/MemberRepository.java
+++ backend/src/main/java/codesquard/app/domain/member/MemberRepository.java
@@ -3,8 +3,7 @@ package codesquard.app.domain.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import codesquard.app.domain.member.Member;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	Member findMemberByNicknameIs(String nickname);
 }
