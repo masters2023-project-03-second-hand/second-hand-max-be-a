b8ff98c361bc77631a5b0e9c53361a655d553e8c
yonghwankim <dragonbead95@naver.com>
2023-08-28 16:18:52 +0900

    #4 test: 로그인 아이디가 중복되는 경우의 테스트 코드 추가

--- backend/src/test/java/codesquard/app/api/oauth/OauthServiceTest.java
+++ backend/src/test/java/codesquard/app/api/oauth/OauthServiceTest.java
@@ -3,12 +3,15 @@ package codesquard.app.api.oauth;
 import static codesquard.app.api.oauth.OauthFixedFactory.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
 import codesquard.app.IntegrationTestSupport;
@@ -20,6 +23,7 @@ import codesquard.app.api.oauth.response.OauthAccessTokenResponse;
 import codesquard.app.api.oauth.response.OauthSignUpResponse;
 import codesquard.app.api.oauth.response.OauthUserProfileResponse;
 import codesquard.app.domain.member.Member;
+import codesquard.app.domain.membertown.MemberTown;
 import codesquard.app.domain.oauth.OauthProvider;
 
 @Transactional
@@ -30,10 +34,11 @@ class OauthServiceTest extends IntegrationTestSupport {
 
 	@DisplayName("로그인 아이디와 소셜 로그인을 하여 회원가입을 한다")
 	@Test
-	public void signUp() {
+	public void signUp() throws IOException {
 		// given
 		String provider = "naver";
 		String code = "1234";
+		MockMultipartFile profile = createFixedProfile();
 		OauthSignUpRequest request = createFixedOauthSignUpRequest();
 		OauthAccessTokenResponse mockAccessTokenResponse = createFixedOauthAccessTokenResponse();
 		OauthUserProfileResponse mockUserProfileResponse = createOauthUserProfileResponse();
@@ -45,17 +50,19 @@ class OauthServiceTest extends IntegrationTestSupport {
 			anyString(), any(OauthProvider.class), any(OauthAccessTokenResponse.class)))
 			.thenReturn(mockUserProfileResponse);
 		// when
-		OauthSignUpResponse response = oauthService.signUp(request, provider, code);
+		OauthSignUpResponse response = oauthService.signUp(profile, request, provider, code);
 		// then
-		Member findMember = memberRepository.findMemberByNicknameIs("23Yong");
+		Member findMember = memberRepository.findMemberByLoginIdIs("23Yong");
 
 		SoftAssertions.assertSoftly(softAssertions -> {
 			softAssertions.assertThat(response)
-				.extracting("avatarUrl", "socialLoginId", "nickname")
-				.containsExactlyInAnyOrder(null, "23Yong1234", "23Yong");
+				.extracting("email", "loginId")
+				.containsExactlyInAnyOrder("23Yong@gmail.com",
+					"23Yong");
 			softAssertions.assertThat(findMember)
-				.extracting("avatarUrl", "socialLoginId", "nickname")
-				.containsExactlyInAnyOrder(null, "23Yong1234", "23Yong");
+				.extracting("email", "loginId")
+				.containsExactlyInAnyOrder("23Yong@gmail.com",
+					"23Yong");
 			softAssertions.assertThat(findMember.getTowns())
 				.hasSize(1)
 				.extracting("name")
@@ -66,13 +73,14 @@ class OauthServiceTest extends IntegrationTestSupport {
 
 	@DisplayName("제공되지 않은 provider로 소셜 로그인하여 회원가입을 할 수 없다")
 	@Test
-	public void signUpWithInvalidProvider() {
+	public void signUpWithInvalidProvider() throws IOException {
 		// given
 		String provider = "github";
 		String code = "1234";
+		MockMultipartFile profile = createFixedProfile();
 		OauthSignUpRequest request = createFixedOauthSignUpRequest();
 		// when
-		Throwable throwable = Assertions.catchThrowable(() -> oauthService.signUp(request, provider, code));
+		Throwable throwable = Assertions.catchThrowable(() -> oauthService.signUp(profile, request, provider, code));
 		// then
 		Assertions.assertThat(throwable)
 			.isInstanceOf(RestApiException.class)
@@ -83,10 +91,11 @@ class OauthServiceTest extends IntegrationTestSupport {
 
 	@DisplayName("잘못된 인가 코드로 소셜 로그인하여 회원가입을 할 수 없다")
 	@Test
-	public void signUpWithInvalidCode() {
+	public void signUpWithInvalidCode() throws IOException {
 		// given
 		String provider = "naver";
 		String code = "1234";
+		MockMultipartFile profile = createFixedProfile();
 		OauthSignUpRequest request = createFixedOauthSignUpRequest();
 		OauthAccessTokenResponse mockAccessTokenResponse = createFixedOauthAccessTokenResponse();
 		// mocking
@@ -97,12 +106,41 @@ class OauthServiceTest extends IntegrationTestSupport {
 			any(OauthAccessTokenResponse.class)))
 			.thenThrow(new RestApiException(OauthErrorCode.WRONG_AUTHORIZATION_CODE));
 		// when
-		Throwable throwable = Assertions.catchThrowable(() -> oauthService.signUp(request, provider, code));
+		Throwable throwable = Assertions.catchThrowable(() -> oauthService.signUp(profile, request, provider, code));
 		// then
 		Assertions.assertThat(throwable)
 			.isInstanceOf(RestApiException.class)
 			.extracting("errorCode")
 			.extracting("name", "httpStatus", "message")
 			.containsExactlyInAnyOrder("WRONG_AUTHORIZATION_CODE", HttpStatus.BAD_REQUEST, "잘못된 인가 코드입니다.");
+	}
+
+	@DisplayName("로그인 아이디가 중복되는 경우 회원가입을 할 수 없다")
+	@Test
+	public void signUpWhenDuplicateLoginId() throws IOException {
+		// given
+		Member member = Member.create("avatarUrlValue", "23Yong1234@gmail.com", "23Yong");
+		MemberTown memberTown = MemberTown.create("가락 1동");
+		member.addMemberTown(memberTown);
+		memberRepository.save(member);
+
+		String provider = "naver";
+		String code = "1234";
+		MockMultipartFile profile = createFixedProfile();
+		OauthSignUpRequest request = createFixedOauthSignUpRequest();
+		OauthAccessTokenResponse mockAccessTokenResponse = createFixedOauthAccessTokenResponse();
+		OauthUserProfileResponse mockUserProfileResponse = createOauthUserProfileResponse();
+		// mocking
+		when(oauthClient.exchangeAccessTokenByAuthorizationCode(
+			any(OauthProvider.class), anyString()))
+			.thenReturn(mockAccessTokenResponse);
+		when(oauthClient.getUserProfileByAccessToken(
+			anyString(), any(OauthProvider.class), any(OauthAccessTokenResponse.class)))
+			.thenReturn(mockUserProfileResponse);
+		// when // then
+		Assertions.assertThatThrownBy(() -> oauthService.signUp(profile, request, provider, code))
+			.isInstanceOf(RestApiException.class)
+			.extracting("errorCode.message")
+			.isEqualTo("이미 존재하는 아이디입니다.");
 	}
 }
