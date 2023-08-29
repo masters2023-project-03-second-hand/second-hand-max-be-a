0fbfe4eab0a9742ac896928bf25d0b60072e0480
yonghwankim <dragonbead95@naver.com>
2023-08-29 15:59:28 +0900

    #5 feat: JWT 객체를 생성하는 클래스 추가

--- backend/src/main/java/codesquard/app/domain/jwt/Jwt.java
+++ backend/src/main/java/codesquard/app/domain/jwt/Jwt.java
@@ -1,4 +1,42 @@
 package codesquard.app.domain.jwt;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class Jwt {
+	private String accessToken;
+	private String refreshToken;
+
+	@JsonIgnore
+	private Date expireDateAccessToken;
+
+	@JsonIgnore
+	private Date expireDateRefreshToken;
+
+	private Jwt() {
+
+	}
+
+	public Jwt(String accessToken, String refreshToken, Date expireDateAccessToken, Date expireDateRefreshToken) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.expireDateAccessToken = expireDateAccessToken;
+		this.expireDateRefreshToken = expireDateRefreshToken;
+	}
+
+	public static Jwt create(String accessToken, String refreshToken, Date expireDateAccessToken,
+		Date expireDateRefreshToken) {
+		return new Jwt(accessToken, refreshToken, expireDateAccessToken, expireDateRefreshToken);
+	}
+
+	@JsonIgnore
+	public long getExpireDateRefreshTokenTime() {
+		return expireDateRefreshToken.getTime();
+	}
 }
