913c0680b4bf094f786c5b2f8181efd438beb6c8
yonghwankim <dragonbead95@naver.com>
2023-08-29 15:50:50 +0900

    #5 feat: 로그인 정보가 일치하지 않을때의 에러 코드 추가

--- backend/src/main/java/codesquard/app/api/errors/errorcode/OauthErrorCode.java
+++ backend/src/main/java/codesquard/app/api/errors/errorcode/OauthErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum OauthErrorCode implements ErrorCode {
 	WRONG_AUTHORIZATION_CODE(HttpStatus.BAD_REQUEST, "잘못된 인가 코드입니다."),
-	NOT_FOUND_PROVIDER(HttpStatus.NOT_FOUND, "provider를 찾을 수 없습니다.");
+	NOT_FOUND_PROVIDER(HttpStatus.NOT_FOUND, "provider를 찾을 수 없습니다."),
+	FAIL_LOGIN(HttpStatus.BAD_REQUEST, "로그인 정보가 일치하지 않습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
