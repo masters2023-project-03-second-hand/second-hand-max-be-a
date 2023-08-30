0978c5f5a43ef5053295079eb1287389a3b55730
yonghwankim <dragonbead95@naver.com>
2023-08-30 17:26:59 +0900

    #5 feat: AuthPrincipalArgumentResolver 리졸버 등록

--- backend/src/main/java/codesquard/app/config/WebConfig.java
+++ backend/src/main/java/codesquard/app/config/WebConfig.java
@@ -2,34 +2,38 @@ package codesquard.app.config;
 
 import java.util.List;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import codesquard.app.api.oauth.resolver.LoginUserArgumentResolver;
 import codesquard.app.domain.jwt.JwtProvider;
+import codesquard.app.domain.oauth.support.AuthPrincipalArgumentResolver;
+import codesquard.app.domain.oauth.support.AuthenticationContext;
 import codesquard.app.filter.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	private final AuthPrincipalArgumentResolver authPrincipalArgumentResolver;
+
+	private final JwtProvider jwtProvider;
+
+	private final AuthenticationContext authenticationContext;
+
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(new LoginUserArgumentResolver());
+		resolvers.add(authPrincipalArgumentResolver);
 		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
 	}
 
 	@Bean
-	public FilterRegistrationBean jwtAuthorizationFilter(JwtProvider provider, ObjectMapper objectMapper,
-		RedisTemplate<String, Object> redisTemplate) {
-		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-		filterFilterRegistrationBean.setFilter(new JwtAuthorizationFilter(provider, objectMapper, redisTemplate));
+	public FilterRegistrationBean<JwtAuthorizationFilter> jwtAuthorizationFilter() {
+		FilterRegistrationBean<JwtAuthorizationFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+		filterFilterRegistrationBean.setFilter(new JwtAuthorizationFilter(jwtProvider, authenticationContext));
 		filterFilterRegistrationBean.addUrlPatterns("/api/*");
 		return filterFilterRegistrationBean;
 	}
