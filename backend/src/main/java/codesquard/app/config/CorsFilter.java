9d3232f70c64fda7fa56c827eb392eb29d1ad20a
yonghwankim <dragonbead95@naver.com>
2023-09-01 13:01:03 +0900

    #6 fix: 오리진 별표로 수정

--- backend/src/main/java/codesquard/app/config/CorsFilter.java
+++ backend/src/main/java/codesquard/app/config/CorsFilter.java
@@ -39,8 +39,7 @@ public class CorsFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse)res;
 
-		response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
-		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "*");
