c220de0a2cefd177250d502cee9dcd8314635930
yonghwankim <dragonbead95@naver.com>
2023-09-05 21:15:36 +0900

    #29 feat: NOT_FOUND_ITEM 에러 코드 추가
    - 상품을 찾지 못할때 발생할 수 있는 에러입니다.

--- backend/src/main/java/codesquard/app/api/errors/errorcode/ItemErrorCode.java
+++ backend/src/main/java/codesquard/app/api/errors/errorcode/ItemErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ItemErrorCode implements ErrorCode {
 
-	INVALID_STATUS(HttpStatus.BAD_REQUEST, "상태는 판매중, 예약중, 판매완료만 들어올 수 있습니다.");
+	INVALID_STATUS(HttpStatus.BAD_REQUEST, "상태는 판매중, 예약중, 판매완료만 들어올 수 있습니다."),
+	NOT_FOUND_ITEM(HttpStatus.NOT_FOUND, "상품을 찾지 못하였습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
