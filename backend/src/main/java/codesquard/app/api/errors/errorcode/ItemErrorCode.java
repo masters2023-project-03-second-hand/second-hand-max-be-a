6aaa223b49c2e58e6dfee076803073d81a0e4416
pie <yejinh_h@naver.com>
2023-09-06 23:19:56 +0900

    #34 feat : 관심상품 등록, 취소에 필요한 에러 구현

--- backend/src/main/java/codesquard/app/api/errors/errorcode/ItemErrorCode.java
+++ backend/src/main/java/codesquard/app/api/errors/errorcode/ItemErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum ItemErrorCode implements ErrorCode {
 
-	INVALID_STATUS(HttpStatus.BAD_REQUEST, "상태는 판매중, 예약중, 판매완료만 들어올 수 있습니다.");
+	INVALID_STATUS(HttpStatus.BAD_REQUEST, "상태는 판매중, 예약중, 판매완료만 들어올 수 있습니다."),
+	ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
