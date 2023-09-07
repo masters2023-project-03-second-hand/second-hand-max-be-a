cb3d0f78e5489896133a84a93d25831154f9a993
pie <yejinh_h@naver.com>
2023-09-07 22:54:18 +0900

    #33 feat : 관심상품 목록 조회 기능 구현

--- backend/src/main/java/codesquard/app/api/wishitem/WishItemController.java
+++ backend/src/main/java/codesquard/app/api/wishitem/WishItemController.java
@@ -1,5 +1,8 @@
 package codesquard.app.api.wishitem;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,7 @@ import codesquard.app.api.errors.exception.RestApiException;
 import codesquard.app.api.response.ApiResponse;
 import codesquard.app.domain.oauth.support.AuthPrincipal;
 import codesquard.app.domain.oauth.support.Principal;
+import codesquard.app.domain.wish.Wish;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,5 +34,11 @@ public class WishItemController {
 			throw new RestApiException(WishErrorCode.INVALID_PARAMETER);
 		}
 		return ApiResponse.ok("관심상품 변경이 완료되었습니다.", null);
+	}
+
+	@GetMapping
+	public ApiResponse<List<Wish>> findAll(@RequestParam(required = false) Long categoryId,
+		@RequestParam(required = false, defaultValue = "10") int size, @RequestParam(required = false) Long cursor) {
+		return ApiResponse.ok("관심상품 조회에 성공하였습니다.", wishItemService.findAll(categoryId, size, cursor));
 	}
 }
--- backend/src/main/java/codesquard/app/domain/wish/WishRepository.java
+++ backend/src/main/java/codesquard/app/domain/wish/WishRepository.java
@@ -1,8 +1,20 @@
 package codesquard.app.domain.wish;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
 
 	void deleteByItemId(Long itemId);
+
+	@Query("SELECT wish FROM Wish wish WHERE wish.id < :cursor")
+	List<Wish> findAll(@Param("cursor") Long cursor, Pageable pageable);
+
+	@Query("SELECT wish FROM Wish wish WHERE wish.id < :cursor AND wish.item.category.id = :categoryId")
+	List<Wish> findAllByCategoryId(@Param("categoryId") Long categoryId, @Param("cursor") Long cursor,
+		Pageable pageable);
 }
