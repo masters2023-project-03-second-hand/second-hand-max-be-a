77ef5c756c7134bbee8b4f69ab0e59c00b4d0406
pie <yejinh_h@naver.com>
2023-09-07 22:53:20 +0900

    #33 refactor : ItemRegisterRequest에 setCategory를 추가하면서 테스트 코드에도 변경이 생겨 수정함

--- backend/src/test/java/codesquard/app/api/item/ItemServiceTest.java
+++ backend/src/test/java/codesquard/app/api/item/ItemServiceTest.java
@@ -8,8 +8,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,23 +15,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import codesquard.app.IntegrationTestSupport;
 import codesquard.app.api.image.ImageUploader;
-import codesquard.app.api.response.ItemListResponse;
 import codesquard.app.api.response.ItemResponse;
+import codesquard.app.api.response.ItemResponses;
+import codesquard.app.domain.category.Category;
 import codesquard.app.domain.item.Item;
 import codesquard.app.domain.member.Member;
 import codesquard.support.SupportRepository;
 
 @SpringBootTest
-class ItemServiceTest {
+class ItemServiceTest extends IntegrationTestSupport {
 
 	@Autowired
 	private ItemService itemService;
-	@Autowired
-	private EntityManager em;
 	@Autowired
 	private SupportRepository supportRepository;
 	@MockBean
@@ -41,18 +38,19 @@ class ItemServiceTest {
 
 	@Test
 	@DisplayName("새로운 상품 등록에 성공한다.")
-	@Transactional
 	void registerTest() {
 		// given
 		given(imageUploader.uploadImageToS3(any(), anyString())).willReturn("url");
-		em.persist(Member.create("avatar", "pie@pie", "pieeeeeee"));
+		Category category = supportRepository.save(Category.create("식품", "~~~~"));
+		Member member = supportRepository.save(Member.create("avatar", "pie@pie", "pieeeeeee"));
+
 		List<MultipartFile> multipartFiles = getMultipartFiles();
 		ItemRegisterRequest request = new ItemRegisterRequest(
-			"선풍기", 12000L, null, "가양 1동", "판매중", 1L, null);
+			"선풍기", 12000L, null, "가양 1동", "판매중", category.getId(), null);
 
 		// when
-		itemService.register(request, multipartFiles, 1L);
-		Item item = em.find(Item.class, 1L);
+		itemService.register(request, multipartFiles, member.getId());
+		Item item = supportRepository.findAll(Item.class).get(0);
 
 		// then
 		assertThat(item.getTitle()).isEqualTo(request.getTitle());
@@ -74,20 +72,21 @@ class ItemServiceTest {
 	@DisplayName("상품 목록 조회에 성공한다.")
 	void findAll() {
 		// given
+		Category category = supportRepository.save(Category.create("식품", "~~~~"));
 		ItemRegisterRequest request1 = new ItemRegisterRequest(
-			"선풍기", 12000L, null, "가양 1동", "판매중", 1L, null);
+			"선풍기", 12000L, null, "가양 1동", "판매중", category.getId(), null);
 		ItemRegisterRequest request2 = new ItemRegisterRequest(
-			"전기밥솥", null, null, "가양 1동", "판매중", 1L, null);
+			"전기밥솥", null, null, "가양 1동", "판매중", category.getId(), null);
 		ItemRegisterRequest request3 = new ItemRegisterRequest(
-			"노트북", null, null, "가양 1동", "판매중", 1L, null);
+			"노트북", null, null, "가양 1동", "판매중", category.getId(), null);
 
 		Member member = supportRepository.save(Member.create("avatar", "pie@pie", "pieeeeeee"));
 		supportRepository.save(request1.toEntity(member, "thumbnail"));
-		supportRepository.save(request2.toEntity(member, "thumbnail"));
+		Item item = supportRepository.save(request2.toEntity(member, "thumbnail"));
 		supportRepository.save(request3.toEntity(member, "thumbnail"));
 
 		// when
-		ItemListResponse all = itemService.findAll("가양 1동", 2, null, null);
+		ItemResponses all = itemService.findAll("가양 1동", 2, null, null);
 
 		// then
 		List<ItemResponse> contents = all.getContents();
@@ -95,6 +94,6 @@ class ItemServiceTest {
 			() -> assertThat(contents.size()).isEqualTo(2),
 			() -> assertThat(contents.get(0).getTitle()).isEqualTo("노트북"),
 			() -> assertThat(all.getPaging().isHasNext()).isTrue(),
-			() -> assertThat(all.getPaging().getNextCursor()).isEqualTo(2L));
+			() -> assertThat(all.getPaging().getNextCursor()).isEqualTo(item.getId()));
 	}
 }
