bc74a468230a6295c1d7dff351cf8a492ea8a3d8
yonghwankim <dragonbead95@naver.com>
2023-09-07 14:58:28 +0900

    #29 fix: 불피요한 주거 제거

--- backend/src/main/java/codesquard/app/domain/jwt/JwtProvider.java
+++ backend/src/main/java/codesquard/app/domain/jwt/JwtProvider.java
@@ -45,18 +45,12 @@ public class JwtProvider {
 	}
 
 	private Jwt createJwt(Map<String, Object> claims, Date expireDateAccessToken, Date expireDateRefreshToken) {
-		// 1. accessToken 생성
 		String accessToken = createToken(claims, expireDateAccessToken);
-
-		// 2. refreshToken 생성
 		String refreshToken = createToken(new HashMap<>(), expireDateRefreshToken);
-
-		// 3. JWT 생성
 		return Jwt.create(accessToken, refreshToken, expireDateAccessToken, expireDateRefreshToken);
 	}
 
 	private String createToken(Map<String, Object> claims, Date expireDate) {
-		// claims를 비밀키로 이용하여 암호화
 		return Jwts.builder()
 			.setClaims(claims)
 			.setExpiration(expireDate)
@@ -64,15 +58,6 @@ public class JwtProvider {
 			.compact();
 	}
 
-	/**
-	 * 액세스 토큰에 해당하는 해시맵 응답
-	 * @UnsupportedJwtException – claimsJws 인수가 Claims JWS를 나타내지 않는 경우
-	 * @MalformedJwtException – claimsJws 문자열이 유효한 JWS가 아닌 경우
-	 * @SignatureException – claimsJws의 JWS 서명이 유효성 검사가 실패하는 경우
-	 * @ExpiredJwtException – 만약 명세된 JWT가 Claims JWT이고 Claims이 만료된 경우
-	 * @IllegalArgumentException – ClaimsJws 문자열이 null, empty, 공백인 경우
-	 * @return token을 비밀키로 복호화한 Claims
-	 */
 	public Claims getClaims(String token) {
 		// token을 비밀키로 복호화하여 Claims 추출
 		try {
