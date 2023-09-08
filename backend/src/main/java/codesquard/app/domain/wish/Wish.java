b571cae159f5e813c7ff49bfc982cd985cb39101
YongHwan Kim <yonghwankim.dev@gmail.com>
2023-09-08 11:29:12 +0900

    #29 fix: 테스트 코드 버그 해결 (#41)
    - 영속성 전이  설정 문제

--- backend/src/main/java/codesquard/app/domain/wish/Wish.java
+++ backend/src/main/java/codesquard/app/domain/wish/Wish.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -12,17 +13,19 @@ import javax.persistence.ManyToOne;
 import codesquard.app.domain.item.Item;
 import codesquard.app.domain.member.Member;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class Wish {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
