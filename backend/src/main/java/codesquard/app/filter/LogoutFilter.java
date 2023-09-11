d3525534afdc9d9307cf36c9fc052f12747076d2
yonghwankim <dragonbead95@naver.com>
2023-09-11 19:06:02 +0900

    #39 feat: 로그아웃 필터 추가
    - 로그아웃 필터는 이미 로그아웃 하였는지 Authorization 헤더를 통해서 엑세스 토큰을 전송하였는지 검사합니다.

---
+++ backend/src/main/java/codesquard/app/filter/LogoutFilter.java
@@ -0,0 +1,90 @@
+package codesquard.app.filter;
+
+import static codesquard.app.filter.JwtAuthorizationFilter.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import codesquard.app.api.errors.errorcode.ErrorCode;
+import codesquard.app.api.errors.errorcode.JwtTokenErrorCode;
+import codesquard.app.api.errors.errorcode.OauthErrorCode;
+import codesquard.app.api.errors.exception.RestApiException;
+import codesquard.app.api.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LogoutFilter extends OncePerRequestFilter {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+	private final ObjectMapper objectMapper;
+
+	private static final AntPathMatcher pathMatcher = new AntPathMatcher();
+	private static final List<String> excludeUrlPatterns = List.of("/api/**");
+
+	@Override
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		return excludeUrlPatterns.stream()
+			.anyMatch(pattern -> pathMatcher.match(pattern, request.getRequestURI()))
+			&& !pathMatcher.match("/api/auth/logout", request.getRequestURI());
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+		if (CorsUtils.isPreFlightRequest(request)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		try {
+			String token = extractJwt(request).orElseThrow(() -> new RestApiException(JwtTokenErrorCode.EMPTY_TOKEN));
+			validateAlreadyLogout(token);
+		} catch (RestApiException e) {
+			setErrorResponse(response, e.getErrorCode());
+			return;
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private void validateAlreadyLogout(String token) {
+		if (Objects.equals(redisTemplate.opsForValue().get(token), "logout")) {
+			throw new RestApiException(OauthErrorCode.ALREADY_LOGOUT);
+		}
+		log.debug("이미 로그아웃 검증 통과 : {}", token);
+	}
+
+	private Optional<String> extractJwt(HttpServletRequest request) {
+		String header = request.getHeader(AUTHORIZATION);
+
+		if (!StringUtils.hasText(header) || !header.startsWith(BEARER)) {
+			return Optional.empty();
+		}
+
+		return Optional.of(header.split(" ")[1]);
+	}
+
+	private void setErrorResponse(HttpServletResponse httpServletResponse, ErrorCode errorCode) throws IOException {
+		httpServletResponse.setStatus(errorCode.getHttpStatus().value());
+		httpServletResponse.setContentType("application/json");
+		httpServletResponse.setCharacterEncoding("UTF-8");
+		ApiResponse<Object> body = ApiResponse.error(errorCode);
+		httpServletResponse.getWriter().write(objectMapper.writeValueAsString(body));
+	}
+}
