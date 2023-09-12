53f9ee6df24724d9259206aa336d0576fb445eb4
yonghwankim <dragonbead95@naver.com>
2023-09-12 16:11:21 +0900

    #49 feat: 동네 삭제 API 추가

--- backend/src/main/java/codesquard/app/api/membertown/MemberTownRestController.java
+++ backend/src/main/java/codesquard/app/api/membertown/MemberTownRestController.java
@@ -3,13 +3,15 @@ package codesquard.app.api.membertown;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import codesquard.app.api.membertown.request.MemberAddRegionRequest;
+import codesquard.app.api.membertown.request.MemberTownAddRequest;
+import codesquard.app.api.membertown.request.MemberTownRemoveRequest;
 import codesquard.app.api.response.ApiResponse;
 import codesquard.app.domain.oauth.support.AuthPrincipal;
 import codesquard.app.domain.oauth.support.Principal;
@@ -27,8 +29,15 @@ public class MemberTownRestController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ApiResponse<Void> addMemberTown(@AuthPrincipal Principal principal,
-		@Valid @RequestBody MemberAddRegionRequest request) {
+		@Valid @RequestBody MemberTownAddRequest request) {
 		memberTownService.addMemberTown(principal, request);
 		return ApiResponse.created("동네 추가에 성공하였습니다.", null);
+	}
+
+	@DeleteMapping
+	public ApiResponse<Void> removeMemberTown(@AuthPrincipal Principal principal,
+		@Valid @RequestBody MemberTownRemoveRequest request) {
+		memberTownService.removeMemberTown(principal, request);
+		return ApiResponse.ok("동네 삭제에 성공하였습니다.", null);
 	}
 }
