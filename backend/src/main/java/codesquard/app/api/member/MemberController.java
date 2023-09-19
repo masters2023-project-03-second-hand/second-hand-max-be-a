fdfff2c7f4c84000ac3ad2d3b7dafb782e09b8f3
pie <yejinh_h@naver.com>
2023-09-19 14:44:06 +0900

    #79 refactor : 리뷰사항 반영

--- backend/src/main/java/codesquard/app/api/member/MemberController.java
+++ backend/src/main/java/codesquard/app/api/member/MemberController.java
@@ -1,7 +1,5 @@
 package codesquard.app.api.member;
 
-import java.util.Map;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import codesquard.app.api.member.response.MemberProfileResponse;
 import codesquard.app.api.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -21,9 +20,9 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PutMapping(value = "/{loginId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ApiResponse<Map<String, String>> modifyProfileImage(@RequestPart MultipartFile updateImageFile,
+	public ApiResponse<MemberProfileResponse> modifyProfileImage(@RequestPart MultipartFile updateImageFile,
 		@PathVariable String loginId) {
 		return ApiResponse.ok("프로필 사진이 수정되었습니다.",
-			Map.of("profileImageUrl", memberService.modifyProfileImage(loginId, updateImageFile)));
+			memberService.modifyProfileImage(loginId, updateImageFile));
 	}
 }
---
+++ backend/src/main/java/codesquard/app/api/member/response/MemberProfileResponse.java
@@ -0,0 +1,11 @@
+package codesquard.app.api.member.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberProfileResponse {
+
+	private String profileImageUrl;
+}
