be7aaee8ed13dbd19976c35e49f51067ca019f75
yonghwankim <dragonbead95@naver.com>
2023-09-21 10:21:19 +0900

    #74 style: 코드 정리

--- backend/src/test/java/codesquard/app/MemberTownTestSupport.java
+++ backend/src/test/java/codesquard/app/MemberTownTestSupport.java
@@ -5,6 +5,7 @@ import codesquard.app.domain.membertown.MemberTown;
 import codesquard.app.domain.region.Region;
 
 public class MemberTownTestSupport {
+	
 	public static MemberTown createMemberTown(Member member, Region region, boolean isSelected) {
 		return new MemberTown(region.getShortAddress(), member, region, isSelected);
 	}
