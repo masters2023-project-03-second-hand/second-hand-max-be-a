e7794c573a3d10fb901a39abc3f051aa6013c997
yonghwankim <dragonbead95@naver.com>
2023-09-30 23:35:16 +0900

    #125 test: 관심상품의 카테고리 조회 목록 테스트 코드 추가

---
+++ backend/src/test/java/codesquard/app/ItemTestSupport.java
@@ -0,0 +1,28 @@
+package codesquard.app;
+
+import static java.time.LocalDateTime.*;
+
+import codesquard.app.domain.category.Category;
+import codesquard.app.domain.item.Item;
+import codesquard.app.domain.item.ItemStatus;
+import codesquard.app.domain.member.Member;
+
+public class ItemTestSupport {
+
+	public static Item createItem(String title, String content, Long price, ItemStatus status, String region,
+		Member member, Category category) {
+		return Item.builder()
+			.title(title)
+			.content(content)
+			.price(price)
+			.status(status)
+			.region(region)
+			.createdAt(now())
+			.wishCount(0L)
+			.viewCount(0L)
+			.chatCount(0L)
+			.member(member)
+			.category(category)
+			.build();
+	}
+}
--- backend/src/test/java/codesquard/app/api/wishitem/WishItemServiceTest.java
+++ backend/src/test/java/codesquard/app/api/wishitem/WishItemServiceTest.java
@@ -1,5 +1,11 @@
 package codesquard.app.api.wishitem;
 
+import static codesquard.app.CategoryTestSupport.*;
+import static codesquard.app.ItemTestSupport.*;
+import static codesquard.app.MemberTestSupport.*;
+import static codesquard.app.MemberTownTestSupport.*;
+import static codesquard.app.RegionTestSupport.*;
+import static codesquard.app.domain.item.ItemStatus.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +13,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,12 +24,20 @@ import org.springframework.test.context.ActiveProfiles;
 import codesquard.app.api.item.request.ItemRegisterRequest;
 import codesquard.app.api.item.response.ItemResponse;
 import codesquard.app.api.item.response.ItemResponses;
+import codesquard.app.api.wishitem.response.WishCategoryListResponse;
 import codesquard.app.domain.category.Category;
+import codesquard.app.domain.category.CategoryRepository;
+import codesquard.app.domain.image.ImageRepository;
 import codesquard.app.domain.item.Item;
 import codesquard.app.domain.item.ItemRepository;
 import codesquard.app.domain.item.ItemStatus;
 import codesquard.app.domain.member.Member;
 import codesquard.app.domain.member.MemberRepository;
+import codesquard.app.domain.membertown.MemberTownRepository;
+import codesquard.app.domain.oauth.support.Principal;
+import codesquard.app.domain.region.Region;
+import codesquard.app.domain.region.RegionRepository;
+import codesquard.app.domain.wish.Wish;
 import codesquard.app.domain.wish.WishRepository;
 import codesquard.app.domain.wish.WishStatus;
 import codesquard.support.SupportRepository;
@@ -42,13 +57,25 @@ class WishItemServiceTest {
 	@Autowired
 	private ItemRepository itemRepository;
 	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private MemberTownRepository memberTownRepository;
+	@Autowired
+	private RegionRepository regionRepository;
+	@Autowired
+	private ImageRepository imageRepository;
+	@Autowired
 	private EntityManager em;
 
 	@AfterEach
 	void tearDown() {
 		wishRepository.deleteAllInBatch();
+		imageRepository.deleteAllInBatch();
 		itemRepository.deleteAllInBatch();
+		categoryRepository.deleteAllInBatch();
+		memberTownRepository.deleteAllInBatch();
 		memberRepository.deleteAllInBatch();
+		regionRepository.deleteAllInBatch();
 	}
 
 	@Test
@@ -154,5 +181,42 @@ class WishItemServiceTest {
 
 		// then
 		assertThat(responses.getContents()).hasSize(2);
+	}
+
+	@DisplayName("한 회원이 등록한 관심 상품들의 중복되지 않은 카테고리를 조회한다")
+	@Test
+	public void readWishCategories() {
+		// given
+		List<Category> categories = categoryRepository.saveAll(List.of(findByName("스포츠/레저"), findByName("가구/인테리어")));
+		List<Member> members = memberRepository.saveAll(List.of(
+			createMember("avatarUrlValue", "23Yong@gmail.com", "23Yong"),
+			createMember("avatarUrlValue", "bruni@gmail.com", "bruni")));
+		List<Region> regions = regionRepository.saveAll(
+			List.of(createRegion("서울 송파구 가락동"), createRegion("서울 종로구 청운동")));
+
+		Member seller = members.get(0);
+		Member buyer = members.get(1);
+		memberTownRepository.saveAll(List.of(
+			createMemberTown(seller, regions.get(0), true),
+			createMemberTown(seller, regions.get(1), false),
+			createMemberTown(buyer, regions.get(0), true),
+			createMemberTown(buyer, regions.get(0), true)));
+		Item item1 = createItem("빈티지 롤러 블레이드", "어린시절 추억의향수를 불러 일으키는 롤러 스케이트입니다.", 200000L, ON_SALE,
+			"가락동", seller, categories.get(0));
+		Item item2 = createItem("빈티지 의자", "의자 팝니다.", 80000L, ON_SALE,
+			"가락동", seller, categories.get(1));
+		itemRepository.saveAll(List.of(item1, item2));
+		wishRepository.saveAll(List.of(new Wish(buyer, item1), new Wish(buyer, item2)));
+		Principal principal = Principal.from(buyer);
+
+		// when
+		WishCategoryListResponse response = wishItemService.readWishCategories(principal);
+
+		// then
+		assertThat(response)
+			.extracting("categories").asList()
+			.extracting("categoryId", "categoryName")
+			.containsExactlyInAnyOrder(Tuple.tuple(1L, "스포츠/레저"), Tuple.tuple(2L, "가구/인테리어"));
+
 	}
 }
