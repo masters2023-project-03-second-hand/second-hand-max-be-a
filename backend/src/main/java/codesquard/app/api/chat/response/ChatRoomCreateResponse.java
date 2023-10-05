1680b970fe156423a04e6e54911c2ffb1a54b5bc
pie <yejinh_h@naver.com>
2023-10-05 16:22:54 +0900

    #151 fix : api 명세서 대로 수정

--- backend/src/main/java/codesquard/app/api/chat/response/ChatRoomCreateResponse.java
+++ backend/src/main/java/codesquard/app/api/chat/response/ChatRoomCreateResponse.java
@@ -1,5 +1,7 @@
 package codesquard.app.api.chat.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import codesquard.app.domain.chat.ChatRoom;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,7 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ChatRoomCreateResponse {
-	
+
+	@JsonProperty(value = "chatRoomId")
 	private Long id;
 
 	public static ChatRoomCreateResponse from(ChatRoom chatRoom) {
