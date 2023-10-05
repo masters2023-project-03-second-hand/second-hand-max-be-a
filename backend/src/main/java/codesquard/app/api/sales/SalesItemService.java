e68147f13502a293b300f2d0dc35f8d6ef55a7d7
pie <yejinh_h@naver.com>
2023-10-05 22:06:40 +0900

    #150 feat : 판매 목록 조회 시 본인이 올린 게시글만 볼 수 있도록validation 추가

--- backend/src/main/java/codesquard/app/api/sales/SalesItemService.java
+++ backend/src/main/java/codesquard/app/api/sales/SalesItemService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import codesquard.app.api.item.response.ItemResponse;
 import codesquard.app.api.item.response.ItemResponses;
+import codesquard.app.domain.oauth.support.Principal;
 import codesquard.app.domain.pagination.PaginationUtils;
 import codesquard.app.domain.sales.SalesPaginationRepository;
 import codesquard.app.domain.sales.SalesStatus;
@@ -18,8 +19,8 @@ public class SalesItemService {
 	private final SalesPaginationRepository salesPaginationRepository;
 
 	@Transactional(readOnly = true)
-	public ItemResponses findAll(SalesStatus status, int size, Long cursor) {
-		Slice<ItemResponse> itemResponses = salesPaginationRepository.findAll(status, size, cursor);
+	public ItemResponses findAll(SalesStatus status, int size, Long cursor, Principal principal) {
+		Slice<ItemResponse> itemResponses = salesPaginationRepository.findAll(status, size, cursor, principal);
 		return PaginationUtils.getItemResponses(itemResponses);
 	}
 }
